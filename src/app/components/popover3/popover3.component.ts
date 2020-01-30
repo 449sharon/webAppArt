104d0aea1f257e606587870ed6d27f16ba40b663
kiwokuhle <39264305+kiwokuhle@users.noreply.github.com>
2020-01-30 17:09:33 +0200

    wish list

---
+++ src/app/components/popover3/popover3.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-popover3',
+  templateUrl: './popover3.component.html',
+  styleUrls: ['./popover3.component.scss'],
+})
+export class Popover3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
