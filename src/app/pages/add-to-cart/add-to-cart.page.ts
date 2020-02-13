71d5b441ff3b5105085219ab24956d3477270e62
Msi87 <51952763+Msi87@users.noreply.github.com>
2020-02-13 13:15:14 +0200

    incoming

--- src/app/pages/add-to-cart/add-to-cart.page.ts
+++ src/app/pages/add-to-cart/add-to-cart.page.ts
@@ -1,3 +1,206 @@
+// import { Component, OnInit } from '@angular/core';
+// import { ModalController, ToastController } from '@ionic/angular';
+// import { BehaviorSubject } from 'rxjs';
+// import * as firebase from 'firebase';
+// import * as moment from 'moment'
+// import { ConfirmationPage } from '../confirmation/confirmation.page';
+// import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+// @Component({
+//   selector: 'app-add-to-cart',
+//   templateUrl: './add-to-cart.page.html',
+//   styleUrls: ['./add-to-cart.page.scss'],
+// })
+// export class AddToCartPage implements OnInit {
+//   private cartItemCount = new BehaviorSubject(0);
+//   private currentNumber: number = 1;
+//   customerUid = firebase.auth().currentUser.uid;
+//   mysize: string = '';
+//   sizes = [];
+//   quantity = 1;
+//   name;
+//   productCode;
+//   key;
+//   total = 0;
+//   myCart: Boolean = false;
+//   // cart = [];
+//   // myArr = [];
+//   amount: number;
+//   dbCart = firebase.firestore().collection('Cart');
+//   dbOrder = firebase.firestore().collection('Order');
+//   dbUser = firebase.firestore().collection('UserProfile');
+//   cartProduct = [];
+//   orderProd = [];
+//   loader: boolean = true;
+//   tempIndex: any;
+//   myProduct: boolean;
+//   id
+//   dataInTheCart = []
+//   constructor(public modalController: ModalController,
+//     public toastCtrl: ToastController) {
+//     this.dbUser.doc(firebase.auth().currentUser.uid).onSnapshot(element => {
+//       console.log(element.data());
+//       this.name = element.data().name
+//     })
+//   }
+//   ionViewWillEnter() {
+//     setTimeout(() => {
+//       this.loader = false;
+//     }, 2000);
+//   }
+//   ngOnInit() {
+
+//     firebase.firestore().collection("Cart").where('uid', '==', firebase.auth().currentUser.uid).get().then((result : any) => {
+//       console.log(result);
+//       //this.myCart
+//       for(let key in result.docs){
+//         this.cartProduct.push(result.docs[key].data())
+//       }
+//       if(this.cartProduct.length > 0){
+//         this.myCart = true
+//       }else{
+//         this.myCart = false
+//       }
+//     }).then( result => {
+//       console.log(this.cartProduct);
+      
+//     })
+
+    
+//     //this.getProducts();
+//     this.trackOrder();
+//   }
+//   ionViewWillLeave() {
+//   }
+//   value
+//   trackOrder() {
+//     this.dbOrder.onSnapshot(res => {
+//     //  console.log('I am a snapshot', res);
+//       for (let key in res.docChanges()) {
+//         let change = res.docChanges()[key]
+//         if (change.type === 'modified') {
+//        //   console.log(change.doc.data().date);
+//           this.value = change.doc.data().date
+//         //  console.log(this.value);
+//         }
+//       }
+//     })
+//   }
+//   getProducts() {
+//     firebase.firestore().collection("Cart").where("customerUid", "==", firebase.auth().currentUser.uid).onSnapshot(snapshot => {
+//       if( this.cartProduct = []){
+//         this.myCart = true;
+//     snapshot.forEach(doc => {
+//       let obj = { obj: doc.data(), id: doc.id }
+//         this.cartProduct.push(obj)
+//        console.log("my products", obj)
+//       });
+//       }else{
+//       }
+     
+  
+//     });
+//   }
+//   plus(prod, index) {
+//     let id = prod.id
+//     this.dbCart.doc(id).update({ quantity: firebase.firestore.FieldValue.increment(1) }).then(res => {
+//     })
+//   }
+//   minus(prod, index) {
+//     let id = prod.id
+//     if (prod.obj.quantity === 1) {
+//      this.toastController("You have reached minimum quantity");
+//     } else {
+//       this.dbCart.doc(id).update({ quantity: firebase.firestore.FieldValue.increment(-1) }).then(res => {
+//       })
+//     }
+//   }
+//   dismiss() {
+//     this.modalController.dismiss({
+//       'dismissed': true
+//     });
+//   }
+//   private increment(p) {
+//     this.currentNumber = this.currentNumber + 1;
+//     this.cartProduct[p].quantity = this.currentNumber
+//   }
+//   private decrement(p) {
+//     if (this.currentNumber > 1) {
+//       this.currentNumber = this.currentNumber - 1;
+//       this.cartProduct[p].quantity = this.currentNumber;
+//     }
+//     return this.currentNumber;
+//   }
+//   decreaseCartItem(p) {
+//     this.cartProduct[p].prod.quantity--;
+//   }
+//   increaseCartItem(p) {
+//     console.log(p);
+//     this.cartProduct[p].prod.quantity++;
+//   }
+//   removeCartItem(id) {
+//     this.dbCart.doc(id).delete();
+//     console.log("I am deleting you", id);
+//   }
+//   getTotal() {
+//     return this.cartProduct.reduce((i, j) => i + j.price * j.quantity, 0);
+//   }
+//   ////////////////////////////////////////////////////////////////////////////////////
+//   //////////////////////// group orders together.
+//   placeOrder() {
+//     let inside = this.getTotal();
+//     console.log('hereTtooo ', inside);
+//     this.orderProd = [];
+//     let key = Math.floor(Math.random() * 100000);
+//     for (let j = 0; j < this.cartProduct.length; j++) {
+//       console.log('Products ', this.cartProduct[j]);
+//       this.orderProd.push(this.cartProduct[j]);
+//     }
+//     if (this.cartProduct.length === 0) {
+//       this.toastController('You cannot place order with empty Cart');
+//     } else {
+//       console.log(inside);
+      
+//       this.dbOrder.doc('Pitseng' + key).set({
+//         totalPrice: inside,
+//         date: moment().format('MMMM Do YYYY, h:mm:ss a'),
+//         product: this.orderProd,
+//         name: this.name,
+//         size: this.sizes,
+//         // productCode:this.productCode,
+//         userID: firebase.auth().currentUser.uid,
+//         pdfLink: "",
+//         // status: 'received',
+//         orderNumber: 'Pitseng' + key
+//       }).then(() => {
+//         this.dbCart.where('customerUid', '==', firebase.auth().currentUser.uid).onSnapshot((res) => {
+//           res.forEach((i) => {
+//             console.log(this.dbCart.doc(i.id));
+            
+//             this.dbCart.doc(i.id).delete();
+//           })
+//         })
+//       })
+//       console.log('My prod ', this.orderProd);
+//       this.dismiss();
+//       this.SuccessModal(key);
+//     }
+//   }
+ 
+//   async toastController(message) {
+//     let toast = await this.toastCtrl.create({ message: message, duration: 2000 });
+//     return toast.present();
+//   }
+//   async SuccessModal(key) {
+//     const modal = await this.modalController.create({
+//       component: ConfirmationPage,
+//       componentProps: { id: key, total: this.total },
+//       cssClass: 'confirmation',
+//     });
+//     return await modal.present();
+//   }
+
+// }
+
 import { Component, OnInit } from '@angular/core';
 import { ModalController, ToastController } from '@ionic/angular';
 import { BehaviorSubject } from 'rxjs';
@@ -5,6 +208,7 @@ import * as firebase from 'firebase';
 import * as moment from 'moment'
 import { ConfirmationPage } from '../confirmation/confirmation.page';
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+
 @Component({
   selector: 'app-add-to-cart',
   templateUrl: './add-to-cart.page.html',
@@ -21,7 +225,7 @@ export class AddToCartPage implements OnInit {
   productCode;
   key;
   total = 0;
-  myCart: Boolean = false;
+  myCart = false;
   // cart = [];
   // myArr = [];
   amount: number;
@@ -34,7 +238,9 @@ export class AddToCartPage implements OnInit {
   tempIndex: any;
   myProduct: boolean;
   id
+
   dataInTheCart = []
+
   constructor(public modalController: ModalController,
     public toastCtrl: ToastController) {
     this.dbUser.doc(firebase.auth().currentUser.uid).onSnapshot(element => {
@@ -42,49 +248,47 @@ export class AddToCartPage implements OnInit {
       this.name = element.data().name
     })
   }
+
   ionViewWillEnter() {
     setTimeout(() => {
       this.loader = false;
     }, 2000);
   }
+
   ngOnInit() {
 
-    firebase.firestore().collection("Cart").where('uid', '==', firebase.auth().currentUser.uid).get().then((result : any) => {
-      console.log(result);
-      //this.myCart
-      for(let key in result.docs){
-        this.cartProduct.push(result.docs[key].data())
-      }
-      if(this.cartProduct.length > 0){
-        this.myCart = true
-      }else{
-        this.myCart = false
-      }
-    }).then( result => {
-      console.log(this.cartProduct);
-      
+    firebase.firestore().collection("Cart").onSnapshot(data => {
+      data.forEach(item => {
+        this.dataInTheCart.push(item.data())
+        console.log("dataInTheCart ", item.data());
+        
+      })
     })
-
-    
-    //this.getProducts();
+    this.getProducts();
     this.trackOrder();
+
   }
+
   ionViewWillLeave() {
+
   }
   value
   trackOrder() {
     this.dbOrder.onSnapshot(res => {
     //  console.log('I am a snapshot', res);
+
       for (let key in res.docChanges()) {
         let change = res.docChanges()[key]
         if (change.type === 'modified') {
        //   console.log(change.doc.data().date);
           this.value = change.doc.data().date
         //  console.log(this.value);
+
         }
       }
     })
   }
+
   getProducts() {
     firebase.firestore().collection("Cart").where("customerUid", "==", firebase.auth().currentUser.uid).onSnapshot(snapshot => {
       if( this.cartProduct = []){
@@ -95,6 +299,7 @@ export class AddToCartPage implements OnInit {
        console.log("my products", obj)
       });
       }else{
+        this.myCart = false;
       }
      
   
@@ -103,7 +308,9 @@ export class AddToCartPage implements OnInit {
   plus(prod, index) {
     let id = prod.id
     this.dbCart.doc(id).update({ quantity: firebase.firestore.FieldValue.increment(1) }).then(res => {
+
     })
+
   }
   minus(prod, index) {
     let id = prod.id
@@ -111,9 +318,11 @@ export class AddToCartPage implements OnInit {
      this.toastController("You have reached minimum quantity");
     } else {
       this.dbCart.doc(id).update({ quantity: firebase.firestore.FieldValue.increment(-1) }).then(res => {
+
       })
     }
   }
+
   dismiss() {
     this.modalController.dismiss({
       'dismissed': true
@@ -123,6 +332,7 @@ export class AddToCartPage implements OnInit {
     this.currentNumber = this.currentNumber + 1;
     this.cartProduct[p].quantity = this.currentNumber
   }
+
   private decrement(p) {
     if (this.currentNumber > 1) {
       this.currentNumber = this.currentNumber - 1;
@@ -133,16 +343,19 @@ export class AddToCartPage implements OnInit {
   decreaseCartItem(p) {
     this.cartProduct[p].prod.quantity--;
   }
+
   increaseCartItem(p) {
     console.log(p);
     this.cartProduct[p].prod.quantity++;
   }
+
   removeCartItem(id) {
     this.dbCart.doc(id).delete();
     console.log("I am deleting you", id);
   }
+
   getTotal() {
-    return this.cartProduct.reduce((i, j) => i + j.price * j.quantity, 0);
+    return this.cartProduct.reduce((i, j) => i + j.obj.price * j.obj.quantity, 0);
   }
   ////////////////////////////////////////////////////////////////////////////////////
   //////////////////////// group orders together.
@@ -158,8 +371,6 @@ export class AddToCartPage implements OnInit {
     if (this.cartProduct.length === 0) {
       this.toastController('You cannot place order with empty Cart');
     } else {
-      console.log(inside);
-      
       this.dbOrder.doc('Pitseng' + key).set({
         totalPrice: inside,
         date: moment().format('MMMM Do YYYY, h:mm:ss a'),
@@ -169,13 +380,11 @@ export class AddToCartPage implements OnInit {
         // productCode:this.productCode,
         userID: firebase.auth().currentUser.uid,
         pdfLink: "",
-        // status: 'received',
+        status: 'received',
         orderNumber: 'Pitseng' + key
       }).then(() => {
         this.dbCart.where('customerUid', '==', firebase.auth().currentUser.uid).onSnapshot((res) => {
           res.forEach((i) => {
-            console.log(this.dbCart.doc(i.id));
-            
             this.dbCart.doc(i.id).delete();
           })
         })
@@ -185,6 +394,8 @@ export class AddToCartPage implements OnInit {
       this.SuccessModal(key);
     }
   }
+
+
   //   if (this.prodCart.length === 0) {
   //     this.toastController('You cannot place order with empty basket');
   //   } else {
@@ -202,7 +413,6 @@ export class AddToCartPage implements OnInit {
   //     })
   //   }
   // }
-
   async toastController(message) {
     let toast = await this.toastCtrl.create({ message: message, duration: 2000 });
     return toast.present();
@@ -215,11 +425,16 @@ export class AddToCartPage implements OnInit {
     });
     return await modal.present();
   }
+
+
   // async createConfirmation() {
   //   const modal = await this.modalController.create({
   //     component:ConfirmationPage,
   //     cssClass: 'confirmation',
+
+
   //   });
   //   return await modal.present();
   // }
+
 }
--- src/app/pages/view-product-details/view-product-details.page.ts
+++ src/app/pages/view-product-details/view-product-details.page.ts
@@ -1,3 +1,417 @@
+// import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+// import { ModalController, ToastController, AlertController, PopoverController } from '@ionic/angular';
+// import { Router } from '@angular/router';
+// import * as firebase from 'firebase';
+// import { BehaviorSubject } from 'rxjs';
+// import { ProductService } from 'src/app/services/product-service.service';
+// import { CartServiceService } from 'src/app/services/cart-service.service';
+// import { Popover2Component } from 'src/app/components/popover2/popover2.component';
+// import { LoginPage } from '../login/login.page';
+// import Swal from 'sweetalert2';
+// import { ThrowStmt } from '@angular/compiler';
+// import { Popover3Component } from 'src/app/components/popover3/popover3.component';
+
+
+// @Component({
+//   selector: 'app-view-product-details',
+//   templateUrl: './view-product-details.page.html',
+//   styleUrls: ['./view-product-details.page.scss'],
+// })
+// export class ViewProductDetailsPage implements OnInit {
+//   cartItemCount:BehaviorSubject<number>;
+//   wishItemCount: BehaviorSubject<number>;
+//   @ViewChild('cart', { static: false, read: ElementRef }) fab: ElementRef;
+//   dbWishlist = firebase.firestore().collection('Wishlist');
+//   dbRating = firebase.firestore().collection('Rating');
+ 
+//   customerUid: any;
+//   dbCart = firebase.firestore().collection('Cart');
+   
+//    currentNumber: number = 1;
+//   Products = [];
+//   proSales = [];
+//   sizes = null;
+//   MyObj = [];
+//   event = {
+//     image: '',
+//     categories: '',
+//     name: '',
+//     price: 0,
+//     productno: '',
+//     desc: null,
+//     small: '',
+//     medium: '',
+//     large: '',
+//     quantity: 1,
+//     amount: 0,
+//     total: 0
+//   };
+//   productSize = {
+//     small: false,
+//     medium: false,
+//     large: false
+//   }
+//   id
+//   image = ""
+//   constructor(public modalController: ModalController,
+//     public productService: ProductService,
+//     public data: ProductService,
+//     public alertCtrl: AlertController,
+//     public toastCtrl: ToastController,
+//     public cartService: CartServiceService,
+//     private router: Router,
+//     public popoverController: PopoverController) { }
+
+//   ngOnInit() {
+//     this.wishItemCount = this.cartService.getWishItemCount();
+//     this.cartItemCount = this.cartService.getCartItemCount();
+//     console.log("Data in the view Details ", this.data.data);
+//     // console.log('$(event)');
+    
+//     // console.log(this.data.data.image);
+//   }
+
+//    increment(p) {
+//     this.currentNumber = this.currentNumber + 1;
+//     this.event.quantity = this.currentNumber
+//   }
+
+//    decrement(p) {
+//     if (this.currentNumber > 1) {
+//       this.currentNumber = this.currentNumber - 1;
+//       this.event.quantity = this.currentNumber;
+//     }
+//     return this.currentNumber;
+//   }
+
+//   ionViewWillEnter(event) {
+//     this.Products.push(this.data.data)
+//     console.log("rrtrtrtrt", this.Products);
+    
+//     this.proSales.push(this.data.data)
+//   }
+//   selectedSize(size) {
+//     let val = size.toElement.value
+//     if (this.sizes == val) {
+//       this.sizes = null
+//     } else {
+//       this.sizes = size.toElement.value
+//     }
+//     console.log(this.sizes);
+
+//     switch (val) {
+//       case 'S':
+//         this.productSize = {
+//           small: true,
+//           medium: false,
+//           large: false
+//         }
+//         break;
+//       case 'M':
+//         this.productSize = {
+//           small: false,
+//           medium: true,
+//           large: false
+//         }
+//         break;
+//       case 'L':
+//         this.productSize = {
+//           small: false,
+//           medium: false,
+//           large: true
+//         }
+//         break;
+//     }
+
+//   }
+
+//   async createModalLogins() {
+//     const modal = await this.modalController.create({
+//       component: LoginPage,
+//       cssClass: 'login-register',
+      
+
+      
+//     });
+//     // this.success()
+//     return await modal.present();
+    
+//   }
+  
+//   addToCart(i) {
+//     // console.log("qqqqqqq  ", i);
+
+//     // let obj = {obj : {
+//     //   categories : i.obj.categories,
+//     //   desc : i.obj.desc,
+//     //   image : i.obj.image,
+//     //   items : i.obj.items,
+//     //   lastcreated : i.obj.lastcreated,
+//     //   name : i.obj.name,
+//     //   price : i.obj.price,
+//     //   productCode : i.obj.productCode,
+//     //   quantity : i.obj.quantity,
+//     //   size : i.obj.size,
+//     //   uid : firebase.auth().currentUser.uid
+//     // }}
+
+//     // firebase.firestore().collection("Cart").doc().set(obj)
+
+//     // setTimeout(() => {
+
+//     //   obj = {obj : {
+//     //     categories :"",
+//     //     desc :"",
+//     //     image  :"",
+//     //     items  :"",
+//     //     lastcreated :"" ,
+//     //     name :"",
+//     //     price  :"",
+//     //     productCode :"",
+//     //     quantity : "" ,
+//     //     size : [],
+//     //     uid : "",
+//     //     status:''
+//     //   }}
+      
+//     // }, 2000);
+
+
+//     // this.dismiss();
+    
+//     if(firebase.auth().currentUser == null) {
+//       console.log('please login');
+//       this.ConfirmationAlert();
+//     this.createModalLogins();
+
+      
+//     }else {
+//       this.customerUid = firebase.auth().currentUser.uid;
+//          let customerUid = firebase.auth().currentUser.uid;
+
+//     console.log(i);
+//     this.dbCart.add({
+//       id: this.id,
+//       timestamp: new Date().getTime(),
+//       customerUid: this.customerUid,
+//        product_name: i.obj.name,
+//       productCode: i.obj.productCode,
+//       desc: i.obj.desc,
+//       status:'received',
+//       size: this.sizes,
+//       price: i.obj.price,
+//       quantity: this.event.quantity,
+//       image: i.obj.image,
+//       amount: i.obj.price * this.event.quantity
+//     })
+//     this.cartItemCount.next(this.cartItemCount.value + 1);
+//     this.dismiss();
+//     this.toastPopover(event)
+//     }
+
+ 
+//   }
+//   getCartItemCount() {
+//     return this.cartItemCount;
+//   }
+//   getWishItemCount() {
+//     return this.cartItemCount;
+//   }
+//   createModalLogin() {
+//     throw new Error("Method not implemented.");
+//   }
+//   toastController(arg0: string) {
+//     throw new Error("Method not implemented.");
+//   }
+
+//   dismiss(){
+//   this.modalController.dismiss({
+//     'dismissed':true
+//   });
+// }
+// logRatingChange(rating, id){
+//  // console.log("changed rating: ",rating);
+//   // do your stuff
+//   this.dbRating.add({
+//     rate: rating,
+//     user: firebase.auth().currentUser.uid,
+//     prod: id
+//   })
+// }
+
+//   ////////
+//   /////
+//   // async toastController(message) {
+//   //   let toast = await this.toastCtrl.create({ message: message, duration: 2000 });
+//   //   return toast.present();
+//   // }
+
+
+//   star1(value, key){
+//     console.log("Method called", key.id, "value ", value); 
+
+   
+  
+//     firebase.firestore().collection("Rating").doc(key.id).set({
+//       id : key,
+//       ratings : value
+//     }, {merge : true})
+
+//   }
+
+
+//   addWishlist(i) {
+
+//     console.log("Method Called ", i);
+//       if(firebase.auth().currentUser == null){
+//          console.log('please like this');
+//          this.ConfirmationAlertWish();
+//          this.createModalLogins()
+//        }else{
+//         this.customerUid = firebase.auth().currentUser.uid;
+//         firebase.firestore().collection("WishList").doc().set({
+  
+
+//             uid : firebase.auth().currentUser.uid,
+//             checked : false,
+//             categories : i.obj.categories,
+//             desc : i.obj.desc,
+//             image : i.obj.image,
+//             items : i.obj.items,
+//             lastcreated : i.obj.lastcreated,
+//             name : i.obj.name,
+//             price : i.obj.price,
+//             productCode : i.obj.productCode,
+//             quantity : i.obj.quantity,
+//             size : i.obj.sizes
+
+         
+//         })
+//         // this.presentToast('ev')
+//        }
+//        this.wishItemCount.next(this.wishItemCount.value + 1);
+//        this.dismiss();
+//   //  if(firebase.auth().currentUser == null){
+//   //    console.log('please like this');
+//   //    this.ConfirmationAlertWish();
+//   //   //  this.createModalLogins()
+
+//   //  }else{
+//   //   this.customerUid = firebase.auth().currentUser.uid; 
+//   //     this.dbWishlist.add({
+//   //     timestamp: new Date().getTime(),
+//   //     customerUid: this.customerUid,
+//   //     product_name: i.name,
+//   //     productCode: i.productCode,
+//   //     desc: i.desc,
+//   //     size: this.sizes,
+//   //     price: i.price,
+//   //     quantity: this.event.quantity,
+//   //     image: i.image,
+//   //     amount: i.price * this.event.quantity
+//   //         // product_name: i.name,
+//   //         // productCode: i.productCode,
+//   //         // size: this.sizes,
+//   //         // price: i.price,
+//   //         // quantity: this.event.quantity,
+//   //         // image: i.image,
+//   //     }).then(() => {
+//   //       this.dismiss();
+//   //       this.presentToast('ev')
+//   //     })
+//   //       .catch(err => {
+//   //         console.error(err);
+//   //       });
+//   //  }
+
+//   //       this.wishItemCount.next(this.wishItemCount.value + 1);
+
+//     } 
+
+
+//     //   if(firebase.auth().currentUser == null) {
+//       // console.log('please login');
+//       // this.ConfirmationAlert();
+//       // this.createModalLogins();
+  
+        
+//       // }else {
+//       //   this.customerUid = firebase.auth().currentUser.uid;
+//   async toastPopover(ev) {
+//     const popover = await this.popoverController.create({
+//       component:Popover2Component,
+//       event: ev,
+      
+//       // cssClass: 'pop-over-style',
+//       translucent: true,
+//     });
+    
+//    popover.present();
+//     setTimeout(()=>popover.dismiss(),500);
+    
+    
+//   }
+//   async presentToast(ev) {
+//     const popover = await this.popoverController.create({
+//       component:Popover3Component,
+//       event: ev,
+      
+//       // cssClass: 'pop-over-style',
+//       translucent: true,
+//     });
+    
+//    popover.present();
+//     setTimeout(()=>popover.dismiss(),500);
+    
+//   }
+
+//   ConfirmationAlert(){
+//     Swal.fire({
+//       title: 'Please login/sign up before adding items to your cart',
+//       showClass: {
+//         popup: 'animated fadeInDown faster'
+//       },
+//       hideClass: {
+//         popup: 'animated fadeOutUp faster'
+//       }
+//     })
+//     this.dismiss()
+//    this.createModalLogins();
+//    }
+
+
+//    ConfirmationAlertWish(){
+//     Swal.fire({
+//       title: 'Please login/sign up before adding items to your wishlist',
+//       showClass: {
+//         popup: 'animated fadeInDown faster'
+//       },
+//       hideClass: {
+//         popup: 'animated fadeOutUp faster'
+//       }
+//     })
+//     this.dismiss()
+//    this.createModalLogins();
+//    }
+
+//    success(){
+//     Swal.fire({
+//       icon: 'success',
+//       title: 'Logged in successfully ',
+//       showClass: {
+//         popup: 'animated fadeInDown faster'
+//       },
+//       hideClass: {
+//         popup: 'animated fadeOutUp faster'
+//       },
+//       showConfirmButton: false,
+//       timer: 500
+//     })
+//    }
+// }
+
+
+
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ModalController, ToastController, AlertController, PopoverController } from '@ionic/angular';
 import { Router } from '@angular/router';
@@ -139,45 +553,6 @@ export class ViewProductDetailsPage implements OnInit {
   }
   
   addToCart(i) {
-    // console.log("qqqqqqq  ", i);
-
-    // let obj = {obj : {
-    //   categories : i.obj.categories,
-    //   desc : i.obj.desc,
-    //   image : i.obj.image,
-    //   items : i.obj.items,
-    //   lastcreated : i.obj.lastcreated,
-    //   name : i.obj.name,
-    //   price : i.obj.price,
-    //   productCode : i.obj.productCode,
-    //   quantity : i.obj.quantity,
-    //   size : i.obj.size,
-    //   uid : firebase.auth().currentUser.uid
-    // }}
-
-    // firebase.firestore().collection("Cart").doc().set(obj)
-
-    // setTimeout(() => {
-
-    //   obj = {obj : {
-    //     categories :"",
-    //     desc :"",
-    //     image  :"",
-    //     items  :"",
-    //     lastcreated :"" ,
-    //     name :"",
-    //     price  :"",
-    //     productCode :"",
-    //     quantity : "" ,
-    //     size : [],
-    //     uid : "",
-    //     status:''
-    //   }}
-      
-    // }, 2000);
-
-
-    // this.dismiss();
     
     if(firebase.auth().currentUser == null) {
       console.log('please login');
@@ -206,7 +581,7 @@ export class ViewProductDetailsPage implements OnInit {
     })
     this.cartItemCount.next(this.cartItemCount.value + 1);
     this.dismiss();
-    this.toastPopover(event)
+    this.toastPopover('ev')
     }
 
  
@@ -247,18 +622,18 @@ logRatingChange(rating, id){
   // }
 
 
+
+  
   star1(value, key){
     console.log("Method called", key.id, "value ", value); 
 
-   
-  
     firebase.firestore().collection("Rating").doc(key.id).set({
-      id : key,
+      
+      id: key,
       ratings : value
-    }, {merge : true})
+    })
 
   }
-
 
   addWishlist(i) {
 
@@ -271,7 +646,7 @@ logRatingChange(rating, id){
         this.customerUid = firebase.auth().currentUser.uid;
         firebase.firestore().collection("WishList").doc().set({
   
-
+          obj : {
             uid : firebase.auth().currentUser.uid,
             checked : false,
             categories : i.obj.categories,
@@ -283,8 +658,8 @@ logRatingChange(rating, id){
             price : i.obj.price,
             productCode : i.obj.productCode,
             quantity : i.obj.quantity,
-            size : i.obj.sizes
-
+            size : i.obj.size
+          },
          
         })
         // this.presentToast('ev')
